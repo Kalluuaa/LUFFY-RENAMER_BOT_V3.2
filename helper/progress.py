547b16b2c9a90707e020b9de57ff7204538f7255
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-11-18 07:11:02 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -23,14 +23,14 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n**Progress**: {2}%\n".format(
-            ''.join(["●" for i in range(math.floor(percentage / 5))]),
-            ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
-            round(percentage, 2))
+        progress = "{0}{1}\n".format(
+            ''.join(["▣" for i in range(math.floor(percentage / 5))]),
+            ''.join(["▢" for i in range(20 - math.floor(percentage / 5))]))
 
-        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+        tmp = progress + "{0} of {1}\n**Progress**: {2}%\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
+            round(percentage, 2),
             humanbytes(speed),
             # elapsed_time if elapsed_time != '' else "0 s",
             estimated_total_time if estimated_total_time != '' else "0 s"
