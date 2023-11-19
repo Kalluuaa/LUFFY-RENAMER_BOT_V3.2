2d73ff0708f5307f1e7e5dd9f58b5b1457cc0e9e
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-11-19 12:03:26 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -3,31 +3,6 @@ import random
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from pyrogram import Client , filters
 from Script import script
-
-@Client.on_callback_query(filters.regex('upgrade'))
-async def upgrade(bot,update):
-	text = """**Free Plan User**
-	Daily  Upload limit 2GB
-	Price 0
-	
-	**VIP 1 ** 
-	Daily  Upload  limit 50GB
-	Price Rs 1231  🇮🇳/🌎 15$  per Year 
-	
-	**VIP 2 **
-	Daily Upload limit Unlimited 
-	Price Rs  2051  🇮🇳/🌎 25$  per Year
-	
-	
-	Pay Using Upi I'd `9480251952@paytm`
-	
-	After Payment Send Screenshots Of 
-        Payment To Admin"""
-	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("⇍ ʙᴀᴄᴋ ⇏",callback_data = "start")  ]])
-	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
