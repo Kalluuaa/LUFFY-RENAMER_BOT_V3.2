7feb3e3c2a6de801e518a6ff2ab17077a9df1784
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-11-21 15:04:06 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -7,25 +7,8 @@ from Script import script
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
-	text = """**Free Plan User**
-	Daily  Upload limit 2GB
-	Price 0
-	
-	**VIP 1 ** 
-	Daily  Upload  limit 50GB
-	Price Rs 1231  🇮🇳/🌎 15$  per Year 
-	
-	**VIP 2 **
-	Daily Upload limit Unlimited
-	Price Rs  2051  🇮🇳/🌎 25$  per Year
-	
-	
-	Pay Using Upi I'd `9480251952@paytm`
-	
-	After Payment Send Screenshots Of 
-        Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await message.reply_text(text = text,reply_markup = keybord)
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/god_luffy_ati")], 
+        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = script.UPGRADE_PLAN_TXT,reply_markup = keybord)
