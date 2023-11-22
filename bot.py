523b10f20e340bc3815c1f28919fabb13dbcba81
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-11-22 09:46:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import asyncio
 from pyrogram import Client, compose,idle
-import os
+import re, os, time
+from datetime import datetime
+from pytz import timezone
+from pyrogram import Client, __version__
+from aiohttp import web
+from pyrogram.raw.all import layer
 
 from plugins.cb_data import app as Client2
 
@@ -11,6 +16,12 @@ API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 
 STRING = os.environ.get("STRING", "")
+
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+
+BOT_UPTIME  = time.time()
+
+WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
 
 
 bot = Client(
@@ -24,6 +35,30 @@ bot = Client(
            api_hash=API_HASH,
 
            plugins=dict(root='plugins'))
+
+
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.mention = me.mention
+        self.username = me.username  
+        self.uptime = BOT_UPTIME     
+        if WEBHOOK:
+            app = web.AppRunner(await web_server())
+            await app.setup()       
+            await web.TCPSite(app, "0.0.0.0", 8080).start()     
+        print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
+        for id in ADMIN:
+            try: await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")                                
+            except: pass
+        if LOG_CHANNEL:
+            try:
+                curr = datetime.now(timezone("Asia/Kolkata"))
+                date = curr.strftime('%d %B, %Y')
+                time = curr.strftime('%I:%M:%S %p')
+                await self.send_message(LOG_CHANNEL, f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\n🉐 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>")                                
+            except:
+                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
            
 
 if STRING:
