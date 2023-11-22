129b9778ecfee1f3d19455b0cb204ed9ba302393
God-Luffy <143315093+God-Luffy@users.noreply.github.com>
2023-11-22 10:24:42 +0530

    I don't know why bot not work

--- bot.py
+++ bot.py
@@ -61,7 +61,7 @@ class Bot(Client):
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
 
 if STRING:
-    apps = [Client2,self]
+    apps = [Client2,Client]
     for app in apps:
         app.start()
     idle()
