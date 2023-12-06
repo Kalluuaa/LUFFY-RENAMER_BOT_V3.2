b5c8c90eaeed09b9a1d68431f8619be0579d7ae4
Kallluaa <136989645+Kallluaa@users.noreply.github.com>
2023-12-06 23:48:34 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -210,100 +210,106 @@ async def cb_handler(client, query: CallbackQuery):
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
 async def send_doc(client,message):
-       update_channel = CHANNEL
-       user_id = message.from_user.id
-       if update_channel :
-       	try:
-       		await client.get_chat_member(update_channel, user_id)
-       	except UserNotParticipant:
-       		await message.reply_text("**__You are not subscribed my channel__** ",
-       		reply_to_message_id = message.id,
-       		reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton("Join Our Update Channel" ,url=f"https://t.me/{update_channel}") ]   ]))
-       		return
-       try:
-           bot_data = find_one(int(botid))
-           prrename = bot_data['total_rename']
-           prsize = bot_data['total_size']
-           user_deta = find_one(user_id)
-       except:
-           await message.reply_text("Use About cmd first /about")
-       try:
-       	used_date = user_deta["date"]
-       	buy_date= user_deta["prexdate"]
-       	daily = user_deta["daily"]
-       	user_type = user_deta["usertype"]
-       except:
-           await message.reply_text("database has been Cleared click on /start")
-           return
-           
-           
-       c_time = time.time()
-       
-       if user_type=="Free":
-           LIMIT = 300
-       else:
-           LIMIT = 10
-       then = used_date+ LIMIT
-       left = round(then - c_time)
-       conversion = datetime.timedelta(seconds=left)
-       ltime = str(conversion)
-       if left > 0:       	    
-       	await message.reply_text(f"`Sorry Dude I am not only for YOU \n Flood control is active so please wait for {ltime}`",reply_to_message_id = message.id)
-       else:
-       		#await client.forward_messages(log_channel, message.from_user.id, message.id)
-       		#await client.send_message(log_channel,f"User Id :- {user_id}")       		
-           		
-       		media = await client.get_messages(message.chat.id,message.id)
-       		file = media.document or media.video or media.audio 
-       		dcid = FileId.decode(file.file_id).dc_id
-       		filename = file.file_name
-       		value = 2147483648
-       		used_ = find_one(message.from_user.id)
-       		used = used_["used_limit"]
-       		limit = used_["uploadlimit"]
-       		expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
-       		if expi != 0:
-       			today = date_.today()
-       			pattern = '%Y-%m-%d'
-       			epcho = int(time.mktime(time.strptime(str(today), pattern)))
-       			daily_(message.from_user.id,epcho)
-       			used_limit(message.from_user.id,0)
-       			if user_type == "NORMAL":
-       				usertype(message.from_user.id,"Free")
-			     		
-       		remain = limit- used
-       		if remain < int(file.file_size):
-       		    await message.reply_text(f"Sorry! I can't upload files that are larger than {humanbytes(limit)}. File size detected {humanbytes(file.file_size)}\nUsed Daly Limit {humanbytes(used)} If U Want to Rename Large File Upgrade Your Plan ",reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upgrade 💰💳",callback_data = "premium") ]]))
-       		    return
-       		if value < file.file_size:
-       		    if STRING:
-       		        if buy_date==None:
-       		            await message.reply_text(f" You Can't Upload More Then {humanbytes(limit)} Used Daly Limit {humanbytes(used)} ",reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("Upgrade 💰💳", callback_data = "premium") ]]))
-       		            return
-       		        pre_check = check_expi(buy_date)
-       		        if pre_check == True:
-       		            await message.reply_text(f"""__𝙒𝙝𝙖𝙩 𝘿𝙤 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝘿𝙤 𝙒𝙞𝙩𝙝 𝙏𝙝𝙞𝙨 𝙁𝙞𝙡𝙚𝙨 ?_\n\n𝐅𝐢𝐥𝐞 𝐍𝐚𝐦𝐞 :- {filename}\n𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞 :- {humanize.naturalsize(file.file_size)}\n𝐃𝐜 𝐈𝐝 :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("🔐 ᴄᴀɴᴄᴇʟ",callback_data = "cancel")  ]]))
-       		            total_rename(int(botid),prrename)
-       		            total_size(int(botid),prsize,file.file_size)
-       		        else:
-       		            backpre(message.from_user.id)
-	
-       		            await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
-       		            return
-       		    else:
-       		          	await message.reply_text("Can't upload files bigger than 2GB ")
-       		          	return
-       		else:
-       		    if buy_date:
-       		        pre_check = check_expi(buy_date)
-       		        if pre_check == False:
-       		            backpre(message.from_user.id)       		            
-       		        
-       		    filesize = humanize.naturalsize(file.file_size)
-       		    fileid = file.file_id
-       		    total_rename(int(botid),prrename)
-       		    total_size(int(botid),prsize,file.file_size)
-       		    await message.reply_text(f"""__𝙒𝙝𝙖𝙩 𝘿𝙤 𝙔𝙤𝙪 𝙒𝙖𝙣𝙩 𝙏𝙤 𝘿𝙤 𝙒𝙞𝙩𝙝 𝙏𝙝𝙞𝙨 𝙁𝙞𝙡𝙚𝙨 ?__\n\n𝐅𝐢𝐥𝐞 𝐍𝐚𝐦𝐞 :- {filename}\n𝐅𝐢𝐥𝐞 𝐒𝐢𝐳𝐞 :- {filesize}\n𝐃𝐜 𝐈𝐝 :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup(
-       		[[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),
-       		InlineKeyboardButton("🔐 ᴄᴀɴᴄᴇʟ",callback_data = "cancel")  ]]))
+    update_channel = CHANNEL
+    user_id = message.from_user.id
+    if update_channel:
+        try:	
+            await client.get_chat_member(update_channel, user_id)
+        except UserNotParticipant:
+            _newus = find_one(message.from_user.id)
+            user = _newus["usertype"]
+            await message.reply_text("**__𝗬𝗼𝘂 𝗔𝗿𝗲 𝗡𝗼𝘁 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲𝗱 𝗠𝘆 𝗖𝗵𝗮𝗻𝗻𝗲𝗹__** ",
+                                     reply_to_message_id=message.id,
+                                     reply_markup=InlineKeyboardMarkup(
+                                         [[InlineKeyboardButton("⚜ Uᴘᴅᴀᴛᴇꜱ Cʜᴀɴɴᴇʟ", url=f"https://t.me/{update_channel}")]]))
+            await client.send_message(log_channel,f"🦋 #rename_logs 🦋,\n**ID** : `{user_id}`\n**Name**: {message.from_user.first_name} {message.from_user.last_name}\n Uꜱᴇʀ-Pʟᴀɴ : {user}\n\n ",
+                                                                                                       reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Rᴇꜱᴛʀɪᴄᴛ Uꜱᴇʀ ( PM ) 🔺", callback_data="ceasepower")]]))
+            return
+
+    try:
+        bot_data = find_one(int(botid))
+        prrename = bot_data['total_rename']
+        prsize = bot_data['total_size']
+        user_deta = find_one(user_id)
+    except:
+        await message.reply_text("Uꜱᴇ Aʙᴏᴜᴛ ᴄᴍᴅ Fɪʀꜱᴛ /about")
+    try:
+        used_date = user_deta["date"]
+        buy_date = user_deta["prexdate"]
+        daily = user_deta["daily"]
+        user_type = user_deta["usertype"]
+    except:
+        await message.reply_text(text=f"Hello dear {message.from_user.first_name}  Wᴇ Aʀᴇ Cᴜʀʀᴇɴᴛʟʏ Wᴏʀᴋɪɴɢ Oɴ Tʜɪꜱ Iꜱꜱᴜᴇ \n\n Pʟᴇᴀꜱᴇ Tʀʏ Tᴏ Rᴇɴᴀᴍᴇ Fɪʟᴇꜱ Fʀᴏᴍ Yᴏᴜʀ Aɴᴏᴛʜᴇʀ Aᴄᴄᴏᴜɴᴛ.\n Bᴇᴄᴀᴜꜱᴇ Tʜɪꜱ BOT Cᴀɴ'ᴛ Rᴇɴᴀᴍᴇ Fɪʟᴇ Sᴇɴᴛ Bʏ Sᴏᴍᴇ Iᴅꜱ.\n\nIғ Yᴏᴜ Aʀᴇ Aɴ ADMIN Dᴏɴ'ᴛ Wᴏʀʀʏ ! Hᴇʀᴇ Wᴇ Hᴀᴠᴇ A Sᴏʟᴜᴛɪᴏɴ Fᴏʀ Yᴏᴜ Dᴇᴀʀ {message.from_user.first_name }.\n\n Pʟᴇᴀꜱᴇ Uꜱᴇ\n👉 `/addpremium ʏᴏᴜʀ_ᴏᴛʜᴇʀ_ᴜꜱᴇʀɪᴅ` 👈 Tᴏ Uꜱᴇ Pʀᴇᴍɪᴜᴍ Fᴇᴀᴜᴛʀᴇ\n\n",
+                                  reply_markup=InlineKeyboardMarkup([
+                                                                     [InlineKeyboardButton("🦋 Contact Owner 🦋", url='https://telegram.me/mr_kallua')]                                                             
+                                                                    ]))
+        await message.reply_text(text=f"🦋")
+        return 
+
+    c_time = time.time()
+
+    if user_type == "Free":
+        LIMIT = 10
+    else:
+        LIMIT = 00
+    then = used_date + LIMIT
+    left = round(then - c_time)
+    conversion = datetime.timedelta(seconds=left)
+    ltime = str(conversion)
+    if left > 0:
+        await message.reply_text(f"```Sᴏʀʀʏ Dᴜᴅᴇ I Aᴍ Nᴏᴛ Oɴʟʏ Fᴏʀ YOU \nFʟᴏᴏᴅ Cᴏɴᴛʀᴏʟ Iꜱ Aᴄᴛɪᴠᴇ Aᴏ Pʟᴇᴀꜱᴇ Wᴀɪᴛ Fᴏʀ {ltime}```", reply_to_message_id=message.id)
+    else:
+        # Forward a single message
+        media = await client.get_messages(message.chat.id, message.id)
+        file = media.document or media.video or media.audio
+        dcid = FileId.decode(file.file_id).dc_id
+        filename = file.file_name
+        value = 2147483648
+        used_ = find_one(message.from_user.id)
+        used = used_["used_limit"]
+        limit = used_["uploadlimit"]
+        expi = daily - int(time.mktime(time.strptime(str(date_.today()), '%Y-%m-%d')))
+        if expi != 0:
+            today = date_.today()
+            pattern = '%Y-%m-%d'
+            epcho = int(time.mktime(time.strptime(str(today), pattern)))
+            daily_(message.from_user.id, epcho)
+            used_limit(message.from_user.id, 0)
+        remain = limit - used
+        if remain < int(file.file_size):
+            await message.reply_text(f"**❗️ 𝟷𝟶𝟶% Oғ Dᴀɪʟʏ {humanbytes(limit)} Dᴀᴛᴀ Qᴜᴏᴛᴀ Exʜᴀᴜꜱᴛᴇᴅ**.\n\n 📁 Fɪʟᴇ Sɪᴢᴇ Dᴇᴛᴇᴄᴛᴇᴅ = {humanbytes(file.file_size)}\n ℹ️ Uꜱᴇᴅ Dᴀɪʟʏ Lɪᴍɪᴛ = {humanbytes(used)}\n\n Yᴏᴜ Hᴀᴠᴇ Oɴʟʏ {humanbytes(remain)} Lᴇғᴛ Oɴ Yᴏᴜʀ Aᴄᴄᴏᴜɴᴛ.\nIғ U Wᴀɴᴛ Tᴏ Rᴇɴᴀᴍᴇ Lᴀʀɢᴇ Fɪʟᴇ Yᴏᴜ Nᴇᴇᴅ Tᴏ Uᴘɢʀᴀᴅᴇ Yᴏᴜʀ Pʟᴀɴ 💡", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Uᴘɢʀᴀᴅᴇ 💰💳", callback_data="upgrade")]]))
+            return
+        if value < file.file_size:
+            
+            if STRING:
+                if buy_date == None:
+                    await message.reply_text(f" Yᴏᴜ Cᴀɴ'ᴛ Uᴘʟᴏᴀᴅ Mᴏʀᴇ Tʜᴇɴ {humanbytes(limit)} Uꜱᴇᴅ Dᴀɪʟʏ Lɪᴍɪᴛ {humanbytes(used)} ", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Uᴘɢʀᴀᴅᴇ 💰💳", callback_data="upgrade")]]))
+                    return
+                pre_check = check_expi(buy_date)
+                if pre_check == True:
+                    await message.reply_text(f"""__Wʜᴀᴛ Dᴏ Yᴏᴜ Wᴀɴᴛ Mᴇ Tᴏ Dᴏ Wɪᴛʜ Tʜɪꜱ Fɪʟᴇ?__\n\n𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲 :- <code>{filename}</code>\n𝗙𝗶𝗹𝗲 𝗦𝗶𝘇𝗲 :- {humanize.naturalsize(file.file_size)}\n𝗗𝗖 𝗜𝗗 :- {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📝 Rᴇɴᴀᴍᴇ", callback_data="rename"), InlineKeyboardButton("✖️ Cᴀɴᴄᴇʟ", callback_data="cancel")]]))
+                    total_rename(int(botid), prrename)
+                    total_size(int(botid), prsize, file.file_size)
+                else:
+                    uploadlimit(message.from_user.id, 1288490188)
+                    usertype(message.from_user.id, "Free")
+
+                    await message.reply_text(f'Yᴏᴜʀ Pʟᴀɴ Exᴘɪʀᴇᴅ Oɴ {buy_date}', quote=True)
+                    return
+            else:
+                await message.reply_text("Cᴀɴ'ᴛ Uᴘʟᴏᴀᴅ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 𝟸GB 😔")
+                return
+        else:
+            if buy_date:
+                pre_check = check_expi(buy_date)
+                if pre_check == False:
+                    uploadlimit(message.from_user.id, 1288490188)
+                    usertype(message.from_user.id, "Free")
+
+            filesize = humanize.naturalsize(file.file_size)
+            fileid = file.file_id
+            total_rename(int(botid), prrename)
+            total_size(int(botid), prsize, file.file_size)
+            await message.reply_text(f"""__Wʜᴀᴛ Dᴏ Yᴏᴜ Wᴀɴᴛ Mᴇ Tᴏ Dᴏ Wɪᴛʜ Tʜɪꜱ Fɪʟᴇ?__\n\n𝗙𝗶𝗹𝗲 𝗡𝗮𝗺𝗲 :- <code>{filename}</code>\n𝗙𝗶𝗹𝗲 𝗦𝗶𝘇𝗲:- {filesize}\n𝗗𝗖 𝗜𝗗 :- {dcid}""", reply_to_message_id=message.id, reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("📝 Rᴇɴᴀᴍᴇ", callback_data="rename"),
+                  InlineKeyboardButton("✖️ Cᴀɴᴄᴇʟ", callback_data="cancel")]]))
